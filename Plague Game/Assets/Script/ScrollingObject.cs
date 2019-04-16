95d282f9289a647a1c1c444748dbbf42a06a0901
Harrison <h.black1@uni.brighton.ac.uk>
2019-04-16 14:55:35 +0100

    terrain update

--- Plague Game/Assets/Script/ScrollingObject.cs
+++ Plague Game/Assets/Script/ScrollingObject.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class ScrollingObject : MonoBehaviour
 {
+	private Rigidbody2D rb2d;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D> ();
+		rb2d.velocity = new Vector2();
     }
 
     // Update is called once per frame
