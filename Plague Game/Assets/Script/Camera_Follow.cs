3f0fc881aa7642f4842b85d67afe8757ab613c30
Harrison <h.black1@uni.brighton.ac.uk>
2019-04-18 11:32:10 +0100

    camera follow

---
+++ Plague Game/Assets/Script/Camera_Follow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Follow : MonoBehaviour
+{
+    public Transform mainplayer;
+	
+	void FixedUpdate()
+	{
+		transform.position = new Vector3(mainplayer.position.x, mainplayer.position.y, transform.position.z);
+	}	
+}
