57f18a46354750e9f3b12a1c58e030cc8127a5a3
Harrison <h.black1@uni.brighton.ac.uk>
2019-04-18 14:12:22 +0100

    background moving

---
+++ Plague Game/Assets/Script/ScrollingBackground.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour
+{
+	public float size;
+    private Transform cameraTransform;
+	private Transform[] no_of_layers;
+	private float view = 10;
+	private int left;
+	private int right;	
+	
+	private void Start()
+	{
+		cameraTransform = Camera.main.transform;
+		no_of_layers = new Transform[transform.childCount];
+		
+		for(int i=0; i<transform.childCount; i++)		
+			no_of_layers[i] = transform.GetChild(i);
+		
+		
+		left = 0;
+		right = no_of_layers.Length-1;		
+	}	
+	
+	private void Update()
+	{
+		if(cameraTransform.position.x < (no_of_layers[left].transform.position.x + view))
+			ScrollLeft();
+		
+		if(cameraTransform.position.x > (no_of_layers[right].transform.position.x - view))
+			ScrollRight();
+	}	
+	
+	private void ScrollRight()
+	{
+		int finalleft = left;
+		no_of_layers[right].position = Vector3.right * (no_of_layers[right].position.x + size);
+		right = left;
+		left = left + 1;
+		
+		if(left == no_of_layers.Length)		
+			left = 0;		
+	}
+	
+	private void ScrollLeft()
+	{
+		int finalright = right;
+		no_of_layers[right].position = Vector3.right * (no_of_layers[left].position.x - size);
+		left = right;
+		right = right - 1;
+		
+		if(right < 0)		
+			right = no_of_layers.Length-1;
+				
+	}	
+}
