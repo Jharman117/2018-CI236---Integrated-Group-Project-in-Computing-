c808091a037625003d45ad5b73aa79716ac31bdc
Harrison <h.black1@uni.brighton.ac.uk>
2019-04-25 14:46:58 +0100

    gravity_attempt

---
+++ Plague Game/Assets/Script/CharacterGravity.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterGravity : MonoBehaviour
+{
+	public float gravitychange = 1f;
+	protected Vector2 velocity;
+	protected Rigidbody2D rb2d;
+	
+	void Enable()
+	{
+		rb2d = GetComponent<Rigidbody2D>();
+	}	
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	void FixedUpdate()
+	{
+		velocity = velocity + (gravitychange * Physics2D.gravity * Time.deltaTime);
+		Vector2 deltaposition = velocity * Time.deltaTime;
+		Vector2 move = Vector2.up * deltaposition.y;
+		Movement(move);
+	}	
+	
+	void Movement(Vector2 move)
+	{
+		rb2d.position = rb2d.position + move;
+	}	
+}
